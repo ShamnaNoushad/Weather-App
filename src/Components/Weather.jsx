ce30314e11cf17daed790b868e7a7acf115cbfc9
ShamnaNoushad <shamna947n@gmail.com>
2024-03-14 14:16:25 +0530

    changed input box

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,10 +1,6 @@
 import React, { useState } from 'react'
 import { Row, Col } from 'react-bootstrap'
-import {
-    MDBInputGroup,
-    MDBIcon
-} from 'mdb-react-ui-kit';
-
+import './weather.css'
 function Weather() {
 
     const [location, setLocation] = useState('')
@@ -35,25 +31,31 @@ function Weather() {
     }
 
     return (
-        <div style={{fontFamily: 'Times New Roman' }}>
+        <div style={{ fontFamily: 'Times New Roman' }}>
             <Row style={{ marginTop: '15%' }}>
 
                 <Col xl={3}> </Col>
 
                 <Col sm={12} md={12} lg={6} xl={6} style={{ color: 'white' }}>
                     <div className="container">
-                    <MDBInputGroup className='mb-5' noBorder>
-                        <input className='form-control' type='text' placeholder='Search' onChange={(e) => locationHandler(e)} />
-                        <button onClick={searchBtn} className='searchBtn' style={{ border: 'none', background: 'transparent',color:'white' }}>
-                            <MDBIcon fas icon='search' />
-                        </button>                    
-                    </MDBInputGroup></div>
+                        <div className="search-container">
+                            <input className="input" type="text" placeholder='Search for weather by location' onChange={(e) => locationHandler(e)}/>
+                            <svg viewBox="0 0 24 24" className="search__icon" onClick={searchBtn}>
+                                <g>
+                                    <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 
+                                    4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z">
+                                    </path>
+                                </g>
+                            </svg>
+                        </div>
+                    </div>
+                  
                     {data.name != undefined && <div>
 
                         <div style={{ textAlign: 'center' }}>
-                            <p style={{ fontWeight: 'bold', fontSize: '25px' }}>{data.name}, {data.sys.country}</p>
-                            {data.main ? <p style={{ fontWeight: 'bolder', fontSize: '50px',textShadow:'2px 2px 4px #000000' }}><span>{data.main.temp}</span>
-                                <span style={{ fontSize: '50px', color: 'orange',textShadow:'2px 2px 4px #000000' }}> °C</span></p> : null}
+                            <p style={{ fontWeight: 'bold', fontSize: '25px' }} className='mt-4'>{data.name}, {data.sys.country}</p>
+                            {data.main ? <p style={{ fontWeight: 'bolder', fontSize: '50px', textShadow: '2px 2px 4px #000000' }}><span>{data.main.temp}</span>
+                                <span style={{ fontSize: '50px', color: 'orange', textShadow: '2px 2px 4px #000000' }}> °C</span></p> : null}
                         </div>
 
                         <div style={{ textAlign: 'center' }}>
